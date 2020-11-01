3838a99afaff72987402dca8eea64aeffb0fbbd6
Caroline Twyman <carolinetwyman@gmail.com>
2020-11-01 17:39:55 -0600

    enabled api-routes, routes functioning

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,63 @@
-// api routes
-// post   /api/workouts
-// put /api/workouts/:id 
-// get /api/workouts/range
+const db = require("../models");
+
+const mongoose = require("mongoose");
+
+module.exports = (app) => {
+
+    //make workout
+
+    app.post("/api/workouts", (req, res) => {
+
+
+        db.Workout.create({}).then(data => res.json(data))
+
+
+        .catch(err => {
+
+            console.log("error", err);
+
+            res.json(err);
+
+          });
+    });
+
+    //add exercise
+
+    app.put("/api/workouts/:id", (req, res) => {
+
+        db.Workout.findByIdAndUpdate(req.params.id,
+
+            {$push: {exercises: req.body}},
+            )
+
+        .then(data => res.json(data))
+
+        .catch(err => {
+
+            console.log("error", err);
+
+            res.json(err);
+
+          });
+
+    });
+
+    //get all workouts in a range
+    app.get("/api/workouts/range", (req, res) => {
+
+
+        db.Workout.find({}).then(data => 
+            res.json(data))
+
+
+        .catch(err => {
+
+            console.log("error", err);
+
+            res.json(err);
+
+          });
+
+    });   
+
+};
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 // routes
 require("./routes/html-routes.js")(app);
-//require("./routes/api-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
